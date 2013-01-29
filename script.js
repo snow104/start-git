9ddfdf3aeb0a606ca243720825f50306892e4fb2
hara <hara@cosa-l.co.jp>
2013-01-29 15:16:39 +0900

    ひな形ファイル投入
    1行空けて詳細メッセージを入力。

---
+++ script.js
@@ -0,0 +1,24 @@
+/*--------------------------------------------------------------------------*
+ *
+ *  名称未設定.js
+ *
+ *  (C) Cosa-L Co.,Ltd.All Rights Reserved.
+ *  
+ *  2012 Cosa-L Co.,Ltd.
+ *  http://www.cosa-l.co.jp/
+ *
+ *  Last Modified: 13/01/29
+ *  Last Editor: Toshiyuki Hara
+ *
+ *  jQuery 1.8.3
+/*--------------------------------------------------------------------------*/
+(function($){
+
+	$(function(){
+
+		// 
+		
+
+	});
+
+})(jQuery);
